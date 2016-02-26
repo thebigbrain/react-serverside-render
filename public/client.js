aa36d786e7b029afa3a262a515a7c94cf256d0e1
jianyi.jiang <jianyi.jiang@uama.com.cn>
2016-02-26 14:56:18 +0800

    ...

--- public/client.js
+++ public/client.js
@@ -1,6 +1,14 @@
-/**
- * New node file
- */
 var content = document.getElementById("content-list");
 
-content.innerHTML = "<ul><li></li></ul>";
+function loadContentDomFromServer(c) {
+	var req = new XMLHttpRequest();
+	req.onload = function(evt){
+		console.log(evt);
+		c.innerHTML = this.responseText;
+	};
+
+	req.open("get", "/api/content-list");
+	req.send();
+};
+
+loadContentDomFromServer(content);
--- server.js
+++ server.js
@@ -3,28 +3,33 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	var options = {
-		root : __dirname + '/public/',
-		dotfiles : 'deny',
-		headers : {
-			'x-timestamp' : Date.now(),
-			'x-sent' : true
-		}
-	};
+    var options = {
+        root: __dirname + '/public/',
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
 
-	var fileName = "index.html";
-	res.sendFile(fileName, options, function(err) {
-		if (err) {
-			console.log(err);
-			res.status(err.status).end();
-		}
-
-	});
+    var fileName = "index.html";
+    res.sendFile(fileName, options, function(err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    });
 });
 
+var router = express.Router();
+router.get("/content-list", function(req, res){
+	res.send("<ul><li></li></ul>");
+});
+
+app.use("/api", router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(80, function() {
-	var port = 80;
-	console.log("start server to listen on port: ", port);
+    var port = 80;
+    console.log("start server to listen on port: ", port);
 });
